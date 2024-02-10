f957e386dc6a95fcc91444d53979c6e2cf68e74f
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2024-02-10 17:12:48 +0100

    added the negative responses

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const yesButton = document.getElementById("yes");
-        const image = document.getElementById("image");
-        const title = document.getElementById("title");
-        let count = 0;
-        yesButton.addEventListener("click", () => {
+    const image = document.getElementById("image");
+    const title = document.getElementById("title");
+    const container = document.getElementById("button-container");
+    let count = 0;
+    yesButton.addEventListener("click", () => {
             // Change the image source to the new image
-            image.src = "./smile.png"; // Set the new image source here
-            title.textContent = "YAYY! ❤️❤️❤️"
-        });
-        const noButton = document.getElementById("no");
-        noButton.addEventListener("click", () => {
-            count += 1;
-            if (count > 1) {
-                
-            }
-        });
+        image.src = "./smile.png"; // Set the new image source here
+        title.textContent = "YAYY! ❤️❤️❤️"
+        container.style.display = "none";
+    });
+    const noButton = document.getElementById("no");
+    noButton.addEventListener("click", () => {
+        count += 1;
+        if (count === 1) {
+           image.src = "./denial.jpg";
+           title.textContent = "Are you sure you don't want to date me? 🥺";      
+           yesButton.innerHTML = "I want to";
+           noButton.innerHTML = "I am sure";
+        }
+        else if (count === 2) {
+            image.src = "./frustration.jpg";
+            title.textContent = "Are you REALLY sure you don't want to date me?";
+            yesButton.va = "I changed my mind";
+            noButton.innerHTML = "I am certain";
+        }
+        else if (count > 2) {
+            image.src = "./damn.gif";
+            title.textContent = "Damn, alright then. Have a nice day.";
+            container.style.display = "none";
+
+        }
+    });
